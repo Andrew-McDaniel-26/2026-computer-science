6908bb0a601f524688704edc655f954409d3dc11
Andrew-McDaniel-26 <mcdanielandrew0528@gmail.com>
2026-09-03 10:15:47 -0500

    Add files via upload
    Spelling corrections and an improved introduction and definitions section

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,14 @@
 // in C++ comments are a // for single line comment and /**/ for multi line comments
 /* this is a multi line comment */
 
-// Andrew McDaniel ,computer science
+// Andrew McDaniel ,Computer Science , 09/03/26, this program caculates the Volume and Surface Area of a cylinder
 #include <iostream>
 // added to include Pi
 #include <numbers>
 
-// the functions used to calculate the indevidual problems Volume and Surface Area
+// assigns a definition to the function calculateVolume
 float calculateVolume(float hight_, float Radius_);
+// assigns a definition to the function calculateSurfaceArea
 float calculateSurfaceArea(float Radius_, float hight_);
 
 int main()
@@ -16,18 +17,16 @@ int main()
 	float hight = 3.5;
 	float Radius = 8;
 
-// the end result is given below
+// the end result is given below, in the form of a scentenes
 		std::cout << "The volume of a cylinder with a hight of " << hight << " and a radius of " << Radius << " is " << calculateVolume(hight, Radius) << std::endl;
 		std::cout << "the Surface Area of a cylinder with the hight of " << hight << " and the Radius of " << Radius << " is " << calculateSurfaceArea(Radius, hight) << std::endl;
 	return 0; 
 }
-// asigns a definition to the function calculateVolume
 float calculateVolume(float hight_, float Radius_)
 {
 	// the calculation for Volume
 	return std::numbers::pi * (Radius_ * Radius_) * hight_;
 }
-// asigns a definition to the function calculateSurfaceArea
 float calculateSurfaceArea(float Radius_, float hight_)
 {
 	// the calculation for Surface Area
